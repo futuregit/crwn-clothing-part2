e2e01af3da77c7de2e852a492bea3f85428cf991
futuregit <tomcis102@hotmail.com>
2022-12-22 22:30:33 -0500

    Added button component

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,19 +1,28 @@
-import { 
-    siginInWithGooglePopup, 
-    createUserDocumentFromAuth  
+
+import {
+    auth,
+    siginInWithGooglePopup,
+    createUserDocumentFromAuth,
+    
 } from '../../utils/firebase/firebase.utils';
- 
+
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+import Button from '../../components/button/button.component';
+
 const SignIn = () => {
+
     const logGoogleUser = async () => {
         const { user } = await siginInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user)
     }
+
     return (
         <div>
             <h1>Sign In</h1>
-            <button onClick={logGoogleUser}>
+            <Button onClick={logGoogleUser} buttonType='google'>
                 Sign in with Google Popup
-            </button>
+            </Button>
+            <SignUpForm />
         </div>
     )
 }
